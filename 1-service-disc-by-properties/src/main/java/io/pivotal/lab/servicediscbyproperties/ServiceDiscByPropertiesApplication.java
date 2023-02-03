60e87bee5304cecc68769f3a0f801f1974fc76f9
Ignacio Lozano <ilozano@vmware.com>
2023-02-03 15:43:25 +0100

    Add SCG getting Active Health Check via properties

---
+++ 1-service-disc-by-properties/src/main/java/io/pivotal/lab/servicediscbyproperties/ServiceDiscByPropertiesApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.lab.servicediscbyproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscByPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscByPropertiesApplication.class, args);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # spring-cloud-gateway-active-health-check
 Blog post describing how to get Active Health Check using Spring Cloud Gateway
+
+
+# TODO
+
+## Service URLs
+* GET localhost:8090/actuator/health
+* GET localhost:8090/hello
+* PUT localhost:8090/status/{true|false}
